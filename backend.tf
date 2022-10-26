3ef522f3606b9cf7c11b2a2889d92d5b819b80de
ikramshahbaz <42874046+ikramshahbaz@users.noreply.github.com>
2022-10-26 18:43:36 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
-/*terraform {
+/*terraform {*/
 backend "azurerm" {
-	resource_group_name  = "New_RG"
+	/*resource_group_name  = "New_RG"
 	storage_account_name = "phoenixteam7892"
 	container_name = "phoenix"
-	key = "terraform.tfstate"
+	key = "terraform.tfstate" */
 	}
-}
-*/
+
+
